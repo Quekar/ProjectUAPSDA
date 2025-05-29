1fe3470af0de474e46066ac983d307732cbab347
KarinaAini <karinaaini354@gmail.com>
2025-05-29 19:05:43 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ class AppTkinter:
         self.root.title("Lorem Ipsum")
         self.root.geometry("1280x720")
         self.root.resizeable(False, False)
+        
+        self.frame_awal = tk.Frame(root, bg="white")
+        self.frame_awal.pack(fill="both", expand=True)
+
+        self.label_nama = tk.Label(self.frame_awal, text="Selamat Datang di Project SDA", font=("Helvetica", 18), bg="white")
+        self.label_nama.pack(pady=50)
+
+        self.btn_masuk = tk.Button(self.frame_awal, text="Masuk", font=("Helvetica", 14), command=self.buka_halaman_utama)
+        self.btn_masuk.pack()
 
 if __name__ == "__main__":
     root = tk.Tk()
