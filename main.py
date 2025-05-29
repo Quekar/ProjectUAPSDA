4f0e8f086f79b57ded768319fc05b2cfc7fc7e3d
Andhika Akbar Pratama <q4quekar@gmail.com>
2025-05-29 16:12:49 +0700

    Update main.py
    membuat dasar aplikasi

--- main.py
+++ main.py
@@ -2,4 +2,14 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from PIL import Image, ImageTk 
 
+class AppTkinter:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Lorem Ipsum")
+        self.root.geometry("1280x720")
+        self.root.resizeable(False, False)
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AppTkinter(root)
+    root.mainloop()
