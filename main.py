4a42e3af32962a7a13fea48e19e4aa4e598d0e0d
Andhika Akbar Pratama <q4quekar@gmail.com>
2025-05-31 05:29:48 +0700

    Update main.py
    mengisi welcome page, memberi 3 button pada welcome page, memberi pesan error untuk button masuk

--- main.py
+++ main.py
@@ -5,18 +5,35 @@ from PIL import Image, ImageTk
 class AppTkinter:
     def __init__(self, root):
         self.root = root
-        self.root.title("Welcome Page")
+        self.root.title("Aplikasi Tkinter")
         self.root.geometry("1280x720")
-        self.root.resizeable(False, False)
+        self.root.resizable(False, False)
+
+        self.background()
+        # klo ada yg mau / niat ganti ke canvas silahkan, gwehj malas
+        self.frame_awal = tk.Frame(self.root, highlightbackground="red" ,highlightcolor="red", bd=0, highlightthickness=5)
+        self.frame_awal.place(relx=0.5, rely=0.5, anchor="center")
         
-        self.frame_awal = tk.Frame(root, bg="white")
-        self.frame_awal.pack(fill="both", expand=True)
+        self.btn_messagebox = tk.Button(self.frame_awal, text="Masuk", font=("Helvetica", 14) command=self.pesan1)
+        self.btn_messagebox.pack(pady=10)
 
-        self.label_nama = tk.Label(self.frame_awal, text="Selamat Datang di Project SDA", font=("Helvetica", 18), bg="white")
-        self.label_nama.pack(pady=50)
+        self.btn_masuk = tk.Button(self.frame_awal, text="Anggota", font=("Helvetica", 14) )
+        self.btn_masuk.pack(pady=10)
+        
+        self.btn_masuk = tk.Button(self.frame_awal, text="Perkenalan", font=("Helvetica", 14) )
+        self.btn_masuk.pack(pady=10)
+        
+        self.frame_utama = tk.Frame(root)
 
-        self.btn_masuk = tk.Button(self.frame_awal, text="Masuk", font=("Helvetica", 14), command=self.buka_halaman_utama)
-        self.btn_masuk.pack()
+    def background(self):
+        img = Image.open("bg.png") # placeholder, bisa diganti
+        img = img.resize((1280, 720))
+        self.photo = ImageTk.PhotoImage(img)
+        self.label_gambar = tk.Label(self.root, image=self.photo)
+        self.label_gambar.place(x=0, y=0, relwidth=1, relheight=1)
+
+    def pesan1(self):
+        messagebox.showinfo("Error", "Maaf, fungsi ini belum dibuat")
 
 if __name__ == "__main__":
     root = tk.Tk()
